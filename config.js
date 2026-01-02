73e6e88df05873afc64e36c5c6ba1dba9878ca84
kingtraderofficial <sblouch400@gmail.com>
2026-01-02 21:39:18 +0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    SECURITY_CODE: "pksignalking900",
-    GEMINI_API_KEY: "AIzaSyBJ9Wdh0diGoj-3W90gmTiN_S1JuwL0dy4",
-    FOREX_API_KEY: "fca_live_sQOckTB4Ofxgsd7YT3cUwQa0W2Bek7scgExmFsia",
-    WHATSAPP: "03064391353",
-    QUOTEX_LINK: "https://broker-qx.pro/sign-up/?lid=1696715"
-};
